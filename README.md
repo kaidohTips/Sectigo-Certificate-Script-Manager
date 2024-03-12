947ba1524ddcb4a30698cf190f22d84ac4327ca9
Kaidoh <kaidohtips@protonmail.com>
2024-03-12 18:38:29 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,224 @@
+<a name="readme-top"></a>
+
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+[![GPLv3][license-shield]][license-url]
+
+
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://github.com/kaidohTips/Sectigo-Certificate-Script-Manager">
+    <img src="lib/images/SCSM_logo.png" alt="Logo">
+  </a>
+
+  <h3 align="center">Sectigo Certificate Script Manager</h3>
+
+  <p align="center">
+    A tool that use the SECTIGO API to manage your certificates !
+    <br />
+    <a href="https://github.com/kaidohTips/Sectigo-Certificate-Script-Manager#Usage"><strong>Explore the docs »</strong></a>
+    <br />
+    <br />
+    <a href="https://github.com/kaidohTips/Sectigo-Certificate-Script-Manager#Demo">View Demo</a>
+    ·
+    <a href="https://github.com/kaidohTips/Sectigo-Certificate-Script-Manager/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/kaidohTips/Sectigo-Certificate-Script-Manager/issues">Request Feature</a>
+  </p>
+</div>
+
+
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">Demo</a>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#roadmap">Roadmap</a></li>
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgments">Acknowledgments</a></li>
+  </ol>
+</details>
+
+
+## Demo
+
+https://github.com/kaidohTips/Sectigo-Certificate-Script-Manager/assets/77806690/5a95c7c9-8852-4c7d-87b2-43e364210871
+
+<!-- ABOUT THE PROJECT -->
+##  🧪 About The Project
+
+As a DevSecOps enthusiast, it's my job to keep the vibes high and maintain the sites in my infrastructure.
+Since I couldn't find any tools that vibed with what I wanted for handling certificates via the Sectigo API, I decided to create my own cool tool.
+
+Please note that I've omitted a substantial portion of the main code for GDPR reasons, so fill free to  <a href="https://github.com/kaidohTips/Sectigo-Certificate-Script-Manager/issues">Request Feature  </a> 
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+### 🎨 Built With
+
+[![python][python-shield]][python-url]
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- GETTING STARTED -->
+## 🔧 Getting Started
+
+First of all, **Make sure to be at least MRAO and be sure that your account is not disabled.**.
+
+The primary administrator privileges and restrictions are divided as follows:
+
+* MRAO administrator — A Master Registration Authority Officer (MRAO) administrator can make changes across all organizations and departments in an enterprise account without any restrictions.
+
+* RAO administrator — A Registration Authority Officer (RAO) administrator can perform operations on specific organizations and departments and for specific certificate types.
+
+* DRAO administrator — A Department Registration Authority Officer (DRAO) can only perform operations on specific departments and for specific certificate types.
+
+
+### 📦️ Prerequisites
+
+* Administrator sectigo account as I said above.
+* Poetry already setup. See [Poetry Installation](https://python-poetry.org/docs/#installation)
+* Python 3.6 or above
+* You must have an .env file at the 
+
+### 🚀 Installation
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/kaidohTips/Sectigo-Certificate-Script-Manager.git &&
+   cd Sectigo-Certificate-Script-Manager
+   ```
+2. Install the dependencies via Poetry
+   ```sh
+   poetry install
+   ```
+3. Enjoy !
+   ```sh
+   python3 scsm.py info yoursite@domain.com
+   ```
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- USAGE -->
+## 📌 Usage
+
+At this point, you can:
+
+| Command    | Description                                |
+|------------|--------------------------------------------|
+| renew      | Renew the certificate                       |
+| info       | Display information of the current certificate |
+| download   | Download certificates (x509CO, x509IOR, and x509co) |
+| deploy     | Deploy the certificate automatically       |
+| create     | Create a certificate                        |
+| update     | Update a certificate                        |
+
+
+
+_For more examples, please refer to the [Documentation](https://example.com)_
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- ROADMAP -->
+## 🔖Roadmap
+
+- [x] Add 'update' command.
+- [ ] CI/CD Pipeline to automatically deploy the certificate in your web server.
+- [ ] Add client certificate management.
+- [ ] ?
+
+See the [open issues](https://github.com/kaidohTips/Sectigo-Certificate-Script-Manager/issues) for a full list of proposed features (and known issues).
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- CONTRIBUTING -->
+## 💚 Contributing
+
+Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+If you have a suggestion that would make this better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
+Don't forget to give the project a ✨!!
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- LICENSE -->
+## 📝 License
+
+Distributed under the GPLv3 License. See [![GPLv3][license-shield]][license-url] for more information.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- CONTACT -->
+## 🧑‍💻 Contact
+
+KaidohTips - kaidohTips@protonmail.com 
+
+Project Link: [SCSM](https://github.com/kaidohTips/Sectigo-Certificate-Script-Manager)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- ACKNOWLEDGMENTS -->
+## 🎉 Acknowledgments
+
+* [Sectigo API](https://www.sectigo.com/knowledge-base/detail/Sectigo-Certificate-Manager-SCM-REST-API/kA01N000000XDkE)
+* [Choose an Open Source License](https://choosealicense.com)
+* [Img Shields](https://shields.io)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+[python-shield]: https://img.shields.io/badge/python--white?style=for-the-badge&logo=python&label=python&color=yellow
+[python-url]: https://python.org
+[contributors-shield]: https://img.shields.io/github/contributors/kaidohTips/Sectigo-Certificate-Script-Manager.svg?style=for-the-badge
+[contributors-url]: https://github.com/kaidohTips/Sectigo-Certificate-Script-Manager/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/kaidohTips/Sectigo-Certificate-Script-Manager.svg?style=for-the-badge
+[forks-url]: https://github.com/kaidohTips/Sectigo-Certificate-Script-Manager/network/members
+[stars-shield]: https://img.shields.io/github/stars/kaidohTips/Sectigo-Certificate-Script-Manager.svg?style=for-the-badge
+[stars-url]: https://github.com/kaidohTips/Sectigo-Certificate-Script-Manager/stargazers
+[issues-shield]: https://img.shields.io/github/issues/kaidohTips/Sectigo-Certificate-Script-Manager.svg?style=for-the-badge
+[issues-url]: https://github.com/kaidohTips/Sectigo-Certificate-Script-Manager/issues
+[license-shield]: https://img.shields.io/github/license/kaidohTips/Sectigo-Certificate-Script-Manager.svg?style=for-the-badge
+[license-url]: https://github.com/kaidohTips/Sectigo-Certificate-Script-Manager?tab=GPL-3.0-1-ov-file#
---
+++ scsm.py
@@ -0,0 +1,287 @@
+#!/usr/bin/python3
+
+"""
+  █████████    █████████   █████████  ██████   ██████
+ ███░░░░░███  ███░░░░░███ ███░░░░░███░░██████ ██████ 
+░███    ░░░  ███     ░░░ ░███    ░░░  ░███░█████░███ 
+░░█████████ ░███         ░░█████████  ░███░░███ ░███ 
+ ░░░░░░░░███░███          ░░░░░░░░███ ░███ ░░░  ░███ 
+ ███    ░███░░███     ███ ███    ░███ ░███      ░███ 
+░░█████████  ░░█████████ ░░█████████  █████     █████
+ ░░░░░░░░░    ░░░░░░░░░   ░░░░░░░░░  ░░░░░     ░░░░░ 
+
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
+"""
+
+import requests
+import argparse
+import os
+from yaspin import yaspin
+import json
+import sys
+import argparse
+import subprocess
+from dotenv import load_dotenv
+from fabric import Connection, task
+import pendulum
+import time
+
+
+load_dotenv()
+
+HEADERS = {
+        'login': os.environ.get("SECTIGO_USER"),
+        'Content-Type': 'application/json;charset=utf-8',
+        'customerUri': os.environ.get("SECTIGO_CUSTOMER_URI"),
+        'password': os.environ.get("SECTIGO_PASS")
+    }
+
+with yaspin() as sp:
+    time.sleep(1)
+
+def GetId(server: str) -> str:
+    try:
+        response = requests.get("https://cert-manager.com/api/ssl/v1", headers=HEADERS, params={'commonName': server})
+        response.raise_for_status()
+        return response.json()[0]['sslId'] 
+    except Exception as e:
+        print("Error",e)
+
+@yaspin(text=f"Binding account to the SECTIGO API ...")
+def BindAPI() -> bool:
+    try:
+        response = requests.get("https://cert-manager.com/api/ssl/v1", headers=HEADERS)
+        if response.status_code == 200:
+            sp.write("Binding to the sectigo API status : [OK]")
+            return True
+        sp.write("Binding to the sectigo API status: [FAIL]")
+        sp.write("Make sure to check your '.env' file or the state of your account in the Sectigo API")
+        return False
+    except Exception as e:
+
+        return False
+
+def GetCertificateInfo(server: str) -> dict[str, str]:
+    response = requests.get(f"https://cert-manager.com/api/ssl/v1/{GetId(server)}", headers=HEADERS, params={'commonName': server})
+    try:
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.HTTPError as e:
+        print(e)
+    except Exception as e:
+        print(e)
+
+@yaspin(text=f"Display Certificate Info ...")
+def DisplayInfo(server: str) -> dict[str, str]:
+    for info_k, info_v in GetCertificateInfo(server).items():
+        sp.write(f"{info_k}: {info_v}")
+    sp.ok("")
+
+def RenewCertificate(server: str) -> bool:
+    """
+    Renew the certificate mapped to the design FQDN 
+    """
+    
+    if options.overwrite:
+        is_renew = input(str(f"Are you sure to renew the certificate of the server '{server}' ? [y/n]"))
+        if is_renew != 'y' or is_renew != 'yes':
+            print(f"Renew Abort ... :(")
+            sys.exit(1)
+    
+    r = requests.post(f"https://cert-manager.com/api/renew/{GetId(server)}", headers=HEADERS, params={'commonName': server})
+    if r.status_code == 200:
+        print(f"The certificate of '{server}' has been renewed '{r.text}'.")
+    else:
+        print(f"Failed to renew certificate of '{server}'")
+        sys.exit(1)
+
+
+def SaveCertificate(content: bytes, filename: str | os.PathLike) -> None:
+    """
+    Save the certificate
+    """
+    with open(filename, 'wb') as file:
+        file.write(content)
+
+@yaspin(text=f"Downloading Certificate ...")
+def DownloadCerts(server: str, _format: list[str], _path: str | os.PathLike) -> None:
+    """
+    Format type for certificate:
+        - Allowed values: 'x509' - for Certificate (w/ chain)
+        - PEM encoded, 'x509CO' - for Certificate only
+        - PEM encoded, 'base64' - for PKCS#7
+        - PEM encoded, 'bin' - for PKCS#7
+        - 'x509IO' - for Root/Intermediate(s) only
+        - PEM encoded
+        - 'x509IOR' - for Intermediate(s)/Root only
+        - PEM encoded, 'pem' - for Certificate (w/ chain)
+        - PEM encoded, 'pemco' - for Certificate only
+        - PEM encoded, 'pemia' - for Certificate (w/ issuer after)
+        - PEM encoded, 'x509R' - for Certificate (w/ chain)
+        - PEM encoded, 'pkcs12' - for Certificate and Private key, PKCS#12. base64 is default.
+    Download certificate
+    """
+
+    TimeOut = 120  # in seconds
+    retry_interval = 5  # in seconds also
+
+    start_time = pendulum.now()
+
+    while pendulum.now().diff(start_time).in_seconds() < TimeOut:
+        
+        if GetCertificateInfo(server)['status'] == "Issued":
+            break
+    else:
+        sp.fail("TimeOut API Down Or there's problem with the status of your certificate")
+        sys.exit(1)
+
+    for f in _format:
+        response = requests.get(f"https://cert-manager.com/api/ssl/v1/collect/{GetId(server)}", headers=HEADERS, params={'commonName': server, 'format': f })
+        cert_name = response.headers.get("Content-Disposition")
+        if cert_name:
+            cert_name = cert_name.split("filename")[1].strip('="')
+        assert cert_name != None
+        sp.write(f"Certificate Name: {cert_name}\n\tFormat: {f}")            
+        SaveCertificate(response.content, f"{_path}/{cert_name}")
+   
+    sp.ok("")
+
+@yaspin(text=f"Updating certificate ...")
+def UpdateCertificate(server: str, **kwargs : dict) -> None:
+    """
+    You can update only certificate fields listed in the table below.
+    Organization ID, Certificate Profile ID, Term, Common name, CSR, SANs can be edited if a certificate state is one of
+        - Requested
+        - Declined
+        - Rejected
+        - Invalid
+    """
+    if GetId(server):
+        payload = {}
+        payload['sslId'] = GetId(server)
+        
+        for key, value in kwargs.items():
+            if key in ["orgId", "certTypeId", "term", "commonName", "csr", "subjectAlternativeNames"] and value is not None:
+                if GetCertificateInfo(server)['status'] != ['Requested','Declined','Rejected','Invalid']:
+                    print(f"Warning ! Status must not be Issued to change: '{key}'")
+                else:
+                    payload[key] = value                
+            else:
+                payload[key] = value                
+        
+        response = requests.put(f"https://cert-manager.com/api/ssl/v1/", headers=HEADERS, json=payload)
+        if response.status_code == 200:
+            sp.ok("")
+        else:
+            sp.fail("")
+
+@yaspin(text=f"Creation of RSA Key ...")
+def NewKey(server: str, size, csr_path: str | os.PathLike, key_path: str | os.PathLike, term, country: str, city: str, org: str):
+    try:
+        new_key = subprocess.run(["openssl","req","-new","-newkey",f"rsa:{size}","-nodes","-days", term, "-out", csr_path + server + ".crt","-keyout", key_path + server + ".key", "-subj", f"/C={country}/ST={state}/L={city}/O={org}/CN={server}"],stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+
+        if new_key.returncode == 0:
+            sp.ok(f"RSA Key of {size} created !")
+            return csr_path + server + ".crt"
+        else:
+            sp.fail("")
+            sys.exit(1)
+            return None
+    except Exception as e:
+        print("Error",e)    
+    
+
+@yaspin(text=f"Creation of Certificate ...")
+def CreationCertificate(server: str, size, csr_path: str | os.PathLike, key_path: str | os.PathLike, term, san: list[str], country: str, city: str, org: str) -> None:
+	if NewKey(server, size, csr_path, key_path, term, country, city, org):
+		with open(csr_path + server + ".crt", 'r') as f:
+			csr_data = f.read()
+			r = requests.post("https://cert-manager.com/api/ssl/v1/enroll", headers=HEADERS, json={'orgId': os.environ.get("SECTIGO_ORG_ID"), 'certType': os.environ.get("SECTIGO_CERT_TYPE_ID"), 'csr': csr_data, 'subjAltNames': ",".join(san) , 'term': term, 'comments': f'Creation of {server} certificate via API','externalRequester':''})
+			print(r.text)
+			if r.status_code == 200:
+				sp.ok("Certificate created!")
+			else:
+				sp.fail("")
+
+
+parser = argparse.ArgumentParser(description="Tools to manage certificate issued from SECTIGO",
+                                 epilog="@kaidohTips",
+                                 formatter_class=argparse.RawTextHelpFormatter)
+
+subparsers = parser.add_subparsers()
+
+parser_bind = subparsers.add_parser("bind",help="test de connectivity with the SECTIGO API")
+
+parser_renew = subparsers.add_parser("renew",help="Renew the certificate")
+parser_renew.add_argument("fqdn",type=str,metavar="FQDN",help="FQDN to renew",nargs="+")
+parser_renew.add_argument("--overwrite","-o",action="store_true",help="Bypass Prompt")
+
+parser_info = subparsers.add_parser("info", help="Display information of the current certificate")
+parser_info.add_argument("fqdn",type=str,metavar="FQDN",help="FQDN of the certificate to display",nargs="+")
+
+parser_download = subparsers.add_parser("download", help="Download certificates (x509CO, x509IOR and x509co)")
+parser_download.add_argument("fqdn", type=str,metavar="Destination Server",help="Server where you want to download the certificate")
+parser_download.add_argument("--format", "-f", type=str, metavar="format",help="Format",default=["x509","x509CO","x509IOR"],nargs="+")
+parser_download.add_argument("--path", "-p", type=str, metavar="DOWNLOAD PATH",help="Path where you'll store your downloaded certificates")
+
+parser_create = subparsers.add_parser("create",help="Create a certificate")
+parser_create.add_argument("fqdn",type=str,help="FQDN Full Qualified Distingued Name")
+parser_create.add_argument("--size",type=str,help="Size of RSA key",default="4096")
+parser_create.add_argument("orgid",type=str,help="OrgID of your domain",default=os.environ.get("SECTIGO_ORG_ID"))
+parser_create.add_argument("cert_type",type=str,help="CertType ID of your domain",default=os.environ.get("SECTIGO_CERT_TYPE_ID"))
+parser_create.add_argument("--csr_path",type=str,help="Path CRT")
+parser_create.add_argument("--key_path",type=str,help="Path KEY")
+parser_create.add_argument("--term",type=str,help="Term (days)",default="365")
+parser_create.add_argument("--SubjectAlternativeNames","--san",type=list[str],help="Subject Alternatives Names of your certificates")
+
+parser_update = subparsers.add_parser("update",help="Update a certificate")
+parser_update.add_argument("fqdn", type=str, help="FQDN Full Qualified Distingued Name")
+parser_update.add_argument("--term", type=int, help="Term (in days)")
+parser_update.add_argument("--certTypeId", type=int, help="Certificate Profile ID", nargs="+")
+parser_update.add_argument("--orgId", type=int, help="Organization ID")
+parser_update.add_argument("--commonName", type=str, metavar="COMMON_NAME", help="Certificate common name")
+parser_update.add_argument("--csr", type=str, metavar="CSR", help="Certificate signing request")
+parser_update.add_argument("--externalRequester", type=str, help="External requester emails, comma-separated")
+parser_update.add_argument("--comments", type=str, metavar="REASON", help="Comments", nargs="?")
+parser_update.add_argument("--subjectAlternativeNames","--san", type=list[str], metavar="SUBJECT ALTERNATIVE NAMES", help="Subject alternative names", nargs="+")
+
+options = parser.parse_args()
+
+
+print("""
+  █████████    █████████   █████████  ██████   ██████
+ ███░░░░░███  ███░░░░░███ ███░░░░░███░░██████ ██████ 
+░███    ░░░  ███     ░░░ ░███    ░░░  ░███░█████░███ 
+░░█████████ ░███         ░░█████████  ░███░░███ ░███ 
+ ░░░░░░░░███░███          ░░░░░░░░███ ░███ ░░░  ░███ 
+ ███    ░███░░███     ███ ███    ░███ ░███      ░███ 
+░░█████████  ░░█████████ ░░█████████  █████     █████
+ ░░░░░░░░░    ░░░░░░░░░   ░░░░░░░░░  ░░░░░     ░░░░░ 
+ 
+ """)
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'renew':
+        RenewCertificate(options.fqdn)
+
+    elif sys.argv[1] == 'bind':
+        BindAPI()
+
+    elif sys.argv[1] == 'info':
+        DisplayInfo(options.fqdn)
+
+    elif sys.argv[1] == 'download':
+        DownloadCerts(options.fqdn,options.format, options.path)
+
+    elif sys.argv[1] == 'update':
+        UpdateCertificate(options.fqdn, **vars(options))
+
+    elif sys.argv[1] == 'create':
+        CreationCertificate(options.fqdn, options.size, options.csr_path, options.key_path, options.term, options.san, options.country, options.state, options.city)
+else:
+    parser.print_help()
